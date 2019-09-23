95ca3e04afc4b38b60d56982cf62e5651acfd716
nitin <nitikavi@gmail.com>
2019-09-23 09:43:54 -0400

    mbh

--- src/test/java/com/intebral/Tests/IntegrationTest.java
+++ src/test/java/com/intebral/Tests/IntegrationTest.java
@@ -2,7 +2,7 @@ package com.intebral.Tests;
 
 public class IntegrationTest {
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
